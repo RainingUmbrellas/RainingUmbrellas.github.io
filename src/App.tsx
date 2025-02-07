0acf5e8352d6613b953d24a5f8e7846a62c4b4e1
RainingUmbrellas <alicia.key21@gmail.com>
2025-02-06 22:44:39 -0500

    Ran Npm deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className='banner'></div>
       <nav></nav>
       <div>
-        <main></main>
+        <main>RainingUmbrellas</main>
         <aside>Bio</aside>
       </div>
       <footer></footer>
