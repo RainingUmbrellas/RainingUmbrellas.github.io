276611d17fb030d45e2ade39fd58b07707e7d792
RainingUmbrellas <alicia.key21@gmail.com>
2025-04-02 18:42:45 -0400

    Deploying Routing changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { HashRouter as Router } from 'react-router-dom';
 
-const root = createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <Router>
     <StrictMode>
       <App />
